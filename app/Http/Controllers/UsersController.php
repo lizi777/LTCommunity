6ccc6dc3ab64ee139321b82c3040750c1fa16e2b
lizi777 <605498134@qq.com>
2018-04-05 12:51:57 +0800

    Image upload and edit authoried

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -14,11 +15,25 @@ class UsersController extends Controller
     }
     public function edit(User $user)
     {
+    	$this->authorize('update', $user);
     	return view('users.edit',compact('user'));
     }
-    public function update(UserRequest $request,User $user)
+    public function update(UserRequest $request,ImageUploadHandler $uploader,User $user)
     {
-    	$user->update($request->all());
+    	$this->authorize('update', $user);
+    	$data = $request->all();
+    	if($request->avatar){
+    		$result = $uploader ->save($request->avatar,'avatars',$user->id);
+    		if($result){
+    			$data['avatar'] = $result['path'];
+    		}
+    	}
+    	$user->update($data);
     	return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功！');
+    }
+//未登录用户只能进入show界面...
+    public function __construct()
+    {
+    	$this->middleware('auth',['except' => ['show']]);
     }
 }
